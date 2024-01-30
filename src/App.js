b3049f93bf31bace16d3a3ee23c42d329c3fd7e2
Sujithup <sujithup24@gmail.com>
2024-01-30 16:26:08 +0530

    Defining Project directories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import styles from "./styles/styles";
 
 function App() {
   return (
-    <div className="App">
-      start
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={styles.boxWidth}>
+          <Navbar />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { navLinks } from "../constants";
+
+function Navbar() {
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+        
+    </nav>
+  );
+}
+
+export default Navbar;
