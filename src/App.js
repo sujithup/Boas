a39aeffb3b2f6998826fe2e4d54fab02eca4a2d8
Sujithup <sujithup24@gmail.com>
2024-02-02 11:00:58 +0530

    Code refactor and Animation

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import "./App.css";
+// App.js:
+
+// Import Components
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+
+// Import sections
 import Billing from "./sections/Billing";
 import Business from "./sections/Business";
 import CardDeal from "./sections/CardDeal";
 import Hero from "./sections/Hero";
 import Stats from "./sections/Stats";
 import Testimonial from "./sections/Testimonial";
+
+// Import styles
 import styles from "./styles/styles";
+import "./App.css";
 
 function App() {
   return (
-    <div className="bg-primary w-full overflow-hidden">
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={styles.boxWidth}>
-          <Navbar />
-        </div>
+    <div className={styles.root}>
+      {/* Navigation Bar */}
+      <div className={styles.navbar}>
+        <Navbar />
       </div>
-
-      <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Hero />
-        </div>
+      {/* Hero Section */}
+      <div className={styles.hero}>
+        <Hero />
       </div>
-
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      {/* Other sections */}
+      <div className={styles.sections}>
         <div className={`${styles.boxWidth}`}>
           <Stats />
           <Business />
-          <Billing/>
-          <CardDeal/>
-          <Testimonial/>
-          <Footer/>
+          <Billing />
+          <CardDeal />
+          <Testimonial />
+          <Footer />
         </div>
       </div>
     </div>
