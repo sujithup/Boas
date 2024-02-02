c96d0366eebaef182361648c9cf638e4f968998d
Sujithup <sujithup24@gmail.com>
2024-02-02 11:02:45 +0530

    Code refactor and Animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ function Navbar() {
 
         <div
           className={`${!toggle ? "hidden" : "flex" } 
-          p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          p-6 bg-black absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
           <ul className="list-none flex justify-end items-start flex-1 flex-col">
             {navLinks.map((nav, index) => (
